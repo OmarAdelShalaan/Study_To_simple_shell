8a7648f8ea6fc7d0c8bfe716777fb17c57251b09
Omar Adel Shalaan <omaradelshalaan@gmail.com>
2023-12-23 03:44:51 +0200

    Add files via upload

--- simple_shell.c
+++ simple_shell.c
@@ -20,71 +20,83 @@ int main(void)
 	char * buffer = NULL;
 	size_t bufsize = 1024;
 	size_t characters = 0;
-	char  argv[10][100] ;
+	char * argv[10];
+//	argv[10]= {ptr1,ptr2,ptr3,ptr4,...,ptr10}
 	int i = 0;
 	// char *const argv[] = {"/usr/bin/ls", "-l", "/media/omar/Hard Disk/ALX_folder", NULL};
 	//char * const argv[] = {"./execve", NULL};
 	//execve(argv[0], argv, NULL);
 	//printf("After execve\n");
 	while(1){
-		// --------------------------get command line -----------------------------------------------------------
-		printf("Type something: ");
-		characters = getline(&buffer,&bufsize,stdin);    //omar\0
-		printf("characters were read: %ld.\n",characters);
-		printf("You typed: %s\n",buffer);
-		//------------------------------------------------------------------------------------------------
-		/* Reallocating memory */
-		//buffer = (char *) realloc(buffer, characters + 5);
-		if(buffer == NULL)
-		{
-			printf("Fauiler Realloc\n");
+		    // --------------------------get command line -----------------------------------------------
+		    printf("Type something: ");
+		    characters = getline(&buffer,&bufsize,stdin);    //omar\n\0==>5
+		    printf("characters were read: %ld.\n",characters);
+		    printf("You typed: %s\n",buffer);
+		    //----------------------------------------------------------------------------------------
+		    /* Reallocating memory */
+		    //buffer = (char *) realloc(buffer, characters + 5);
+		    if(buffer == NULL)
+		    {
+			    printf("Fauiler Realloc\n");
+		    }
+		    // --------------------------Cut Buffer-------------------------------------------
+		    //strcat(buffer, "NULL");
+		    printf("Before Strtok\n");
+		    buffer[characters - 1] = '\0';
+		    buffer = strtok( buffer, " ");
+		    printf("after Strtok\n");
+		    i = 0;
+		    while(buffer != NULL)
+		    {
+		        //strcpy(argv[i] , buffer);
+		        argv[i] = buffer;//omar\0adel\0shalaan\0//	argv[10]= {ptr1,ptr2,ptr3,ptr4,...,ptr10}
+		        printf("buffer: %s,  argv: %s\n",buffer,argv[i]);////omar\0adel\0shalaan\0
+		        buffer = strtok(NULL, " ");////omar\0adel\0shalaan\0
+		                                    ////adel\0shalaan\0
+		        i++;
+		    }
+		    //*argv[i] += "\0";
+		    //strcat(argv[i],"\0");
+		    i = 0;
+		    while(argv[i] != NULL)
+		    {
+		        printf("*****%s\n",argv[i]);
+		        i++;
+		    }
+		    printf("Done argv add\n");
+		    //		char *strtok(char *str, const char *delims);
+		    //char * argv[] = strtok( buffer, " ");
+		    //strtok--------------------------------------------------
+		    //add Path /usr/bin/--------------------------------------$PATHls
+		    
+		    // ------------------fork to child execve----------------------------
+		    fflag = fork();
+		    if (fflag == 0)
+		    {
+		    // --------------------------child execve--------------------
+			    printf("Child\n");
+			    
+			    execve(argv[0], ( char **)argv, NULL);
+		    }
+		    else if (fflag == -1)
+			    printf("fealuire");
+		    else 
+		    {
+			    // --------------------------wait child execve--------------------
+			    wait(NULL);
+			    printf("\n\n\n\nParent\n");
+		    }
+		    
+		    printf("After execve\n");
+		    free(buffer);
+        	i = 0;
+        	while(argv[i] != NULL)
+		    {
+                argv[i] = NULL;
+		        i++;
+		    }
 		}
-		// --------------------------Cut Buffer-----------------------------------------------------------
-		//strcat(buffer, "NULL");
-		printf("Before Strtok\n");
-		buffer = strtok( buffer, " ");
-		printf("after Strtok\n");
-		i = 0;
-		while(buffer != NULL)
-		{
-		    strcpy(argv[i] , buffer);
-		    //printf("%s,     %s\n",buffer,argv[i]);
-		    buffer = strtok(NULL, " ");
-		    i++;
-		}
-		i--;
-		len = strlen(argv[i]);
-		len--;
-		argv[i][len] = '\0';
-		//printf("%s\n",argv[i]);
-		i++;
-		//*argv[i] += "\0";
-		strcat(argv[i],"\0");
-		printf("Done argv add\n");
-		//		char *strtok(char *str, const char *delims);
-		//char * argv[] = strtok( buffer, " ");
-		//strtok--------------------------------------------------
-		//add Path /usr/bin/--------------------------------------$PATHls
-		
-		// --------------------------fork to child execve-----------------------------------------------------------
-		fflag = fork();
-		if(fflag ==0)
-		{
-		// --------------------------child execve--------------------
-			printf("Child\n");
-			execve(argv[0], argv, NULL);
-		}
-		else if(fflag == -1)
-			printf("fealuire");
-		else 
-		{
-			// --------------------------wait child execve--------------------
-			wait(NULL);
-			printf("\n\n\n\nParent\n");
-		}
-		
-		printf("After execve\n");
-    	}
 	return (0);
 }
 
